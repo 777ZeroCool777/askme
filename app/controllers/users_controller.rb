52ecde1f24455b9ea557bcedff85da5df92c2727
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-06-17 01:27:52 +0300

    fix add logged after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: 'Пользователь успешно зарегестрирован!'
     else
       render 'new'
