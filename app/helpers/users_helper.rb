40da4223f1796b13ee0d4641471b2ab8b12f6a12
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-06-17 00:15:57 +0300

    add questions table and sklonyator

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+  # метод для склонения вопросов
+  def question_sklonyator(question)
+    ostatok = question.size % 10
+
+    ostatok100 = question.size % 100 # исключения для цифр от 11 до 14 родительный падеж (Кого? Чего?)
+
+    if question.size == 0
+      "Нет вопросов"
+    elsif ostatok100 >= 11 && ostatok100 <= 14
+      "#{question.size} вопросов"
+    elsif ostatok == 1
+      "#{question.size} вопрос"
+    elsif ostatok >= 2 && ostatok <= 4
+      "#{question.size} вопроса"
+    elsif ostatok > 4 || ostatok == 0
+      "#{question.size} вопросов"
+    end
+  end
 end
