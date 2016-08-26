fe8c71ea99a7008a7854a98d3cc430fbf1752272
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-08-26 23:40:30 +0300

    add migration for capcha

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731120627) do
+ActiveRecord::Schema.define(version: 20160826203801) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
     t.string   "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
     t.integer  "author_id"
+    t.integer  "questioning_user_id"
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
