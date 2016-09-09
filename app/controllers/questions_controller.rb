4546b0e8e51f746d2ead4a455c2caa74558623d5
ZeroCool <zerocool@zerocool-VirtualBox.(none)>
2016-09-09 13:29:08 +0300

    recaptcha refactor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,5 @@
-﻿# (c) goodprogrammer.ru
-#
-# Контроллер, управляющий вопросами
-# 1. Редактирует вопрос
-# 2. Создает вопрос
-# 3. Удаляет вопрос
-#
-class QuestionsController < ApplicationController
-
-  # инициализируем объект вопрос для экшенов кроме :edit, :update, :destroy
+﻿class QuestionsController < ApplicationController
   before_action :load_question, only: [:edit, :update, :destroy]
-
-  # проверяем имеет ли юзер доступ к экшену
   before_action :authorize_user, except: [:create]
 
   # GET /questions/1/edit
@@ -22,7 +11,6 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.questioning_user = current_user if current_user.present?
 
-    # К сохранению вопроса добавляем проверку капчи
     if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
@@ -48,8 +36,6 @@ class QuestionsController < ApplicationController
   end
 
   private
-
-  # загружаем вопрос
   def load_question
     @question = Question.find(params[:id])
   end
@@ -58,9 +44,7 @@ class QuestionsController < ApplicationController
     reject_user unless @question.user == current_user
   end
 
-
   def question_params
-    # защита от уязвимости -- пользователь может менять ответы только у собственных вопросов
     if current_user.present? && params[:question][:user_id].to_i == current_user.id
       params.require(:question).permit(:user_id, :text, :answer)
     else
@@ -68,7 +52,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # Метод, который проверяет капчу с использованием гема recaptcha
   def check_captcha(model)
     verify_recaptcha(model: model) unless current_user.present?
   end
